8ec0822a408f51150dd900d22cc05a22834e8927
Sara <saracoppola996@gmail.com>
2023-07-24 19:43:26 +0200

    first commit

---
+++ src/House.java
@@ -0,0 +1,14 @@
+public class House {
+
+String address;
+int numberOfFloors;
+
+House(String address, int numberOfFloors){
+    this.address = address;
+    this.numberOfFloors = numberOfFloors;
+
+
+
+}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*Constructor
+        Create a class called House that has 3 instance variables:
+        a string address
+        an int numberOfFloors
+        Define a constructor method for House that will print in console a message as soon as a House object is created
+        Define a testing class (e.g. Tester, Start or another reasonable name you prefer) with a main method where:
+        you declare two House variables house1 and house2
+        you create two House objects and assign them to the variables house1 and house2*/
+
+public class Main {
+    public static void main(String[] args) {
+       House house1 = new House("Via San Paolo", 2);
+       House house2 = new House("Via Vittorio Emanuele", 6 );
+
+       System.out.println("House 1 " + "is in " + house1.address + " and has " + house1.numberOfFloors + " floors");
+       System.out.println("House 2 " + "is in " + house2.address + " and has " + house2.numberOfFloors + " floors");
+    }
+}
